b1f62e7b4c68083a382f419b5b1af232e7dd1516
vaishnavi <vaishnavi.k@dhatvibs.com>
2026-06-09 10:48:38 +0530

    schama changes

--- config/firebase.js
+++ config/firebase.js
@@ -1,28 +1,13 @@
-const admin = require("firebase-admin");
+const { initializeApp, getApps, cert } = require("firebase-admin/app");
 
-
-
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
+if (!getApps().length) {
+  initializeApp({
+    credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   });
 }
 
-
-//testing
-
-// console.log("Firebase apps:", admin.apps.length);
-
-// console.log("FIREBASE_PROJECT_ID:", process.env.FIREBASE_PROJECT_ID);
-// console.log("FIREBASE_CLIENT_EMAIL:", process.env.FIREBASE_CLIENT_EMAIL);
-// console.log(
-//   "FIREBASE_PRIVATE_KEY exists:",
-//   !!process.env.FIREBASE_PRIVATE_KEY
-// );
-
-module.exports = admin;
+module.exports = { initializeApp };
